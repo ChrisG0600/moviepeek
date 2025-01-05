7c610907b776715d4414645e8988991aa9aa8fc3
Chrstian Goyagoy <c.goyagoy41@gmail.com>
2025-01-05 23:24:41 +0800

    Sample Landing Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+$(document).ready(function() {
+    $('#mobileMenuButton').on('click', function() {
+        $('#mobileMenu').toggleClass('hidden');
+        $('#menuIcon').toggleClass('hidden');
+        $('#closeIcon').toggleClass('hidden');
+    });
+});
